c549402572da9276ff70295ce23a59918a6adf7e
Iamneo <techadmin@iamneo.ai>
2023-09-27 16:43:48 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import './App.css';
+import BlogApp from './Components/BlogApp';
+
+function App() {
+  return (
+    <BlogApp />
+  );
+}
+
+export default App;
